1e4ca3d979f80d12c0e8a7d69e39895670809b6a
hnanmal <60164230+hnanmal@users.noreply.github.com>
2022-11-23 23:11:05 +0900

    asdfaf

--- src/show_date_app/core.clj
+++ src/show_date_app/core.clj
@@ -2,33 +2,44 @@
   (:gen-class))
 
 (import
-  '(java.util Date)
-  '(java.awt FlowLayout)
-  '(javax.swing JLabel JFrame)
-  '(java.text SimpleDateFormat))
+ '(java.util Date)
+ '(java.awt FlowLayout)
+;;  '(java.awt Dimension)
+ '(javax.swing JLabel JFrame JPanel JButton)
+ '(java.text SimpleDateFormat))
 
 (defn getTime []
   (Date.))
 
 (defn editTime [x]
-  (.format (SimpleDateFormat. "yyyy년 MM월 dd일") (getTime)))
+  (.format (SimpleDateFormat. "yyyy년 MM월 dd일") x))
 
 (defn show-window []
   (let [window (JFrame. "날짜 띄우기")
+        panel (JPanel.)
+        btn1 (JButton. "Click Me")
         text (JLabel. (editTime (getTime)))]
-      (doto window
-          (.setLayout (FlowLayout.))
-          (.getContentPane)
-          (.add text)
-          (.setDefaultCloseOperation (JFrame/EXIT_ON_CLOSE))
-          (.pack)
-          (.setSize 300 200)
-          (.setVisible true))))
+    
+    (doto window
+      (.add
+       (.add panel (JLabel. "Today is"))
+       )
+      (.setLayout (FlowLayout.))
+      (.getContentPane)
+      (.add text)
+      (.add btn1)
+      (.setDefaultCloseOperation (JFrame/EXIT_ON_CLOSE))
+      (.pack)
+      ;; (.setPreferredSize (Dimension. 400 400))
+      (.setSize 400 300)
+      (.setLocationRelativeTo nil)
+      (.setResizable false)
+      (.setVisible true))))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
 
-  (show-window)
+  (show-window))
   
-  )
+  
